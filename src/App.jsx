b7195068957536bee3fcaac2f8f32a82ea8c879b
liliyafedyurina <liliya.fedyurina@gmail.com>
2025-03-27 13:39:49 -0700

    weatherService added, App.js modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 // src/App.jsx
-
+import { useState } from 'react';
+import * as weatherService from './services/weatherService';
 const App = () => {
+const fetchData = async () => {
+  const data = await weatherService.show('Seattle');
+  console.log('Data ', data);
+}
 
   return (
+    <>
     <h1>Hello world!</h1>
+    <button onClick={fetchData}>Fetch Weather Data</button>
+    </>
   );
 }
 
